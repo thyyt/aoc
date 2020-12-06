e248674bb78fa7418bb2f94037803f14a47bc75a
Tatu Hyytiäinen <tatu.hyytiainen@unity3d.com>
2020-12-06 21:21:22 +0200

    Solution for part 2

--- day_4/solution.py
+++ day_4/solution.py
@@ -1,4 +1,5 @@
 from typing import List, Dict
+import re
 
 REQUIRED_FIELDS = [
     "byr",
@@ -10,6 +11,52 @@ REQUIRED_FIELDS = [
     "pid",
 ]
 OPTIONAL_FIELD = "cid"
+ALLOWED_HAIR_COLORS = [
+    "amb",
+    "blu",
+    "brn",
+    "gry",
+    "grn",
+    "hzl",
+    "oth",
+]
+
+
+def country_id_validator(value: str) -> bool:
+    return True
+
+
+def birth_year_validator(value: str) -> bool:
+    return int(value) >= 1920 and int(value) <= 2002
+
+
+def issue_year_validator(value: str) -> bool:
+    return int(value) >= 2010 and int(value) <= 2020
+
+
+def expiration_year_validator(value: str) -> bool:
+    return int(value) >= 2020 and int(value) <= 2030
+
+
+def height_validator(value: str) -> bool:
+    height_value = value[:-2]
+    if value[-2:] == "in":
+        return int(height_value) >= 59 and int(height_value) <= 76
+    if value[-2:] == "cm":
+        return int(height_value) >= 150 and int(height_value) <= 193
+    return False
+
+
+def hair_color_validator(value: str) -> bool:
+    return bool(re.match("^#[a-z0-9]{6}$", value))
+
+
+def eye_color_validator(value: str) -> bool:
+    return value in ALLOWED_HAIR_COLORS
+
+
+def passport_id_validator(value: str) -> bool:
+    return bool(re.match("^[a-z0-9]{9}$", value))
 
 
 def parse_passports(lines: List[str]) -> List[Dict[str, str]]:
@@ -24,13 +71,32 @@ def parse_passports(lines: List[str]) -> List[Dict[str, str]]:
         else:
             parsed_passports.append(current_passport)
             current_passport = {}
+    parsed_passports.append(current_passport)
     return parsed_passports
 
 
-def is_valid_passport(pw_fields: Dict[str, str]) -> bool:
+FIELD_VALIDATOR_MAP = {
+    "byr": birth_year_validator,
+    "iyr": issue_year_validator,
+    "eyr": expiration_year_validator,
+    "hgt": height_validator,
+    "hcl": hair_color_validator,
+    "ecl": eye_color_validator,
+    "pid": passport_id_validator,
+    "cid": country_id_validator,
+}
+
+
+def fields_are_valid(pw_fields: Dict[str, str]) -> bool:
+    for key, value in pw_fields.items():
+        if not FIELD_VALIDATOR_MAP[key](value):
+            return False
+    return True
+
+
+def has_required_fields(pw_fields: Dict[str, str]) -> bool:
     for field in REQUIRED_FIELDS:
         if not (field in pw_fields.keys()):
-            print(field, len(pw_fields), pw_fields)
             return False
     return True
 
@@ -44,15 +110,20 @@ def read_input(input_file) -> List[str]:
 def main():
     lines = read_input("day_4/input.txt")
     passport_data = parse_passports(lines)
+    has_fields_counter = 0
     valid_passports = 0
-    total_passports = 0
+    invalid_passports = []
     for passport in passport_data:
-        valid = is_valid_passport(passport)
-        valid_passports += valid
-        total_passports += 1
+        has_fields = has_required_fields(passport)
+        valid = fields_are_valid(passport)
+        has_fields_counter += has_fields
+        valid_passports += valid and has_fields
 
+        if not valid:
+            invalid_passports.append(passport)
+
+    print("Required fields in passports: ", has_fields_counter)
     print("Valid passports: ", valid_passports)
-    print("Total passports: ", total_passports)
 
 
 if __name__ == "__main__":
