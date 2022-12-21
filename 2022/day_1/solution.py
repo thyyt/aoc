c1bf80911753e75dbfe991c3788942492944d7cc
Tatu Hyytiäinen <tatu.hyytiainen@unity3d.com>
2022-12-21 08:46:27 +0200

    Day 1 2022

---
+++ 2022/day_1/solution.py
@@ -0,0 +1,46 @@
+from typing import List, NoReturn, Tuple, Set
+
+
+def read_input(input_file) -> List[str]:
+    open_file = open(input_file, "rb+")
+    lines = open_file.readlines()
+    return [line.decode("utf-8").strip() for line in lines]
+
+
+def first_part(input: List[str]) -> int:
+    current_calories = 0
+    max_calories = 0
+    for calories in input:
+        if calories:
+            current_calories += int(calories)
+        else:
+            max_calories = max(max_calories, current_calories)
+            current_calories = 0
+
+    max_calories = max(max_calories, current_calories)
+    return max_calories
+
+
+def second_part(input: List[str]) -> int:
+
+    current_calories = 0
+    elf_calories = []
+    for calories in input:
+        if calories:
+            current_calories += int(calories)
+        else:
+            elf_calories.append(current_calories)
+            current_calories = 0
+
+    elf_calories.append(current_calories)
+
+    return sum(sorted(elf_calories)[-3:])
+
+def main():
+    input = read_input("input")
+    print(first_part(input))
+    print(second_part(input))
+
+
+if __name__ == "__main__":
+    main()
